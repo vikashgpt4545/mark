05cc516e37a5f451b8a1ceb5bd1f28033dfe048a
Vikash Mahto <vikashmahto45@gmail.com>
2026-08-12 20:51:49 +0530

    Fix unclosed HTML tag in finance/index.php to resolve build failure

--- finance/index.php
+++ finance/index.php
@@ -113,6 +113,7 @@ include __DIR__ . '/../components/header.php';
                         <li><a href="../calculators/savings.php">Savings Planner</a></li>
                         <li><a href="../loans/personal-loans.php">Personal Loan Rates</a></li>
                         <li><a href="../credit-cards/credit-score.php">Credit Score Guide</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
